009ef6a945a19171b3475311b555a78945a1c20c
aakankshavaishnav <aakanksha.vaishnav.cg@gmail.com>
2026-09-22 16:19:42 +0530

    added js

--- assignment.js
+++ assignment.js
@@ -1,2 +1,31 @@
-let prompt=require("enetr your num: ")
-console.log(prompt)
+
+// let prompt = require("prompt-sync")();
+// let age = prompt("enter your age:")
+// console.log(`my age is ${age}`)
+
+// console.log(5===6)
+// let a = 5;
+// let b = "5";
+// let result = a !== b;
+// console.log('a === b:', result); // false (different types)
+// console.log(10 !==10)
+//  == => only values
+// === values+ data types
+// console.log(false==0)
+// console.log(null==undefined)
+// console.log(NaN==undefined)
+// console.log({}==[])
+// let scannedProduct="Abc20";
+// let storedProduct="Abc20";
+// let products=scannedProduct===storedProduct
+// console.log(products)
+// let selectedCountrycode="+91"
+// let savedCountrycode="+10"
+// let isDifferent=(selectedCountrycode!==savedCountrycode)
+// console.log(isDifferent)
+// let currentDevice="speaker"
+// let registeredDevice="mic"
+// let isDifferent=(currentDevice!==registeredDevice)
+// console.log(isDifferent)
+
+
---
+++ ques.js
@@ -0,0 +1,19 @@
+// let storedPassword=1234;
+// let enteredPassword="1234";
+// // let res=storedPassword===enteredPassword
+// console.log(res)
+// let storePin=23223;
+// let enterPin=23223;
+// let result=(storePin==enterPin);
+// console.log( "the eneterd pin is",result)
+//question=2
+//verify that a saved theme prefrence (e.g "drak" )matches the current slelection
+// let savedTheme="dark";
+// let currentTheme="dark";
+// let selection=savedTheme==currentTheme
+// console.log("current selection match saved selection",selection) 
+//question=3 if saved language is not equal to browser laanguage!!
+let savedLanguage=22;
+let BrowserLanguage=12;
+let currentCode =savedLanguage!=BrowserLanguage
+console.log(currentCode)
